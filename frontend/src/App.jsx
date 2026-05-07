8e13508b422cafcb4666bafd8758ca77945515ca
matthewndirangu <ndirangumatthew9@gmail.com>
2026-05-07 18:46:14 +0300

    chore: install and confiqure Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return(
-    <div>
-      <h1>SkyDesk</h1>
-      <p>Airline Ticket Management System</p>
+    <div className ="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-blue-400">✈ SkyDesk</h1>
+        <p className="text-gray-400 mt-2">Airline Ticket Management System</p>
+      </div> 
     </div>
   )
 }
